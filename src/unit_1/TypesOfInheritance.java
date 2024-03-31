1e990c093312c2a89e76fbc9ed5534d51d579fdf
Varun <varunjethani@outlook.com>
2024-03-31 20:58:24 +0530

    p7

--- src/unit_1/TypesOfInheritance.java
+++ src/unit_1/TypesOfInheritance.java
@@ -7,6 +7,11 @@ abstract class ElectricVehicle{
         this.range = range;
         this.capacity = capacity;
     }
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Range: " + range);
+        System.out.println("Capacity: " + capacity);
+    }
 
 }
 
@@ -14,16 +19,46 @@ class ElectricScooter extends ElectricVehicle{
     public ElectricScooter(String name, int range, int capacity) {
         super(name, range, capacity);
     }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("This is a Scooter");
+    }
 }
+
 
 class ElectricCar extends ElectricVehicle{
     public ElectricCar(String name, int range, int capacity) {
         super(name, range, capacity);
     }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("This is a Car");
+    }
 }
 
-class ElectricPickupTruck{
+class ElectricPickupTruck extends ElectricCar{
+    public ElectricPickupTruck(String name, int range, int capacity) {
+        super(name, range, capacity);
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("This is a Pickup Truck");
+    }
 
 }
 public class TypesOfInheritance {
+    public static void main(String[] args) {
+        ElectricVehicle vehicle = new ElectricScooter("Ather", 100, 2);
+        vehicle.display();
+        vehicle = new ElectricCar("Nexon", 200, 4);
+        vehicle.display();
+        vehicle = new ElectricPickupTruck("CyberTruck", 300, 6);
+        vehicle.display();
+    }
 }
