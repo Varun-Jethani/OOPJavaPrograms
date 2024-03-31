b0e228eff41896153872ac610c93726c346b693e
Varun <varunjethani@outlook.com>
2024-04-01 01:16:20 +0530

    update StringOperations

--- src/unit_1/StringOperations.java
+++ src/unit_1/StringOperations.java
@@ -1,21 +1,38 @@
 package unit_1;
 
 public class StringOperations {
+
+    public static void print(String str) {
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
         String str = "Hello World";
-        System.out.println("Length of the string is "+str.length());
-        System.out.println("Character at index 4 is "+str.charAt(4));
-        System.out.println("Substring from index 2 to 5 is "+str.substring(2,5));
-        System.out.println("Index of 'o' is "+str.indexOf('o'));
-        System.out.println("Index of 'o' after 5 is "+str.indexOf('o',5));
-        System.out.println("Index of 'o' from last is "+str.lastIndexOf('o'));
-        System.out.println("Index of 'o' from last before 5 is "+str.lastIndexOf('o',5));
-        System.out.println("Concatenation with 'Java' is "+str.concat(" Java"));
-        System.out.println("Concatenation with 'Java' is "+str+" Java");
-        System.out.println("Uppercase is "+str.toUpperCase());
-        System.out.println("Lowercase is "+str.toLowerCase());
-        System.out.println("Trimmed string is "+str.trim());
-        System.out.println("Replaced 'o' with 'a' is "+str.replace('o','a'));
-        System.out.println("Replaced 'World' with 'Java' is "+str.replace("World","Java"));
+        print("Length of the string is "+str.length());
+        print("Character at index 4 is "+str.charAt(4));
+        print("Substring from index 2 to 5 is "+str.substring(2,5));
+        print("Index of 'o' is "+str.indexOf('o'));
+        print("Index of 'o' after 5 is "+str.indexOf('o',5));
+        print("Index of 'o' from last is "+str.lastIndexOf('o'));
+        print("Index of 'o' from last before 5 is "+str.lastIndexOf('o',5));
+        print("Concatenation with 'Java' is "+str.concat(" Java"));
+        print("Concatenation with 'Java' is "+str+" Java");
+        print("Uppercase is "+str.toUpperCase());
+        print("Lowercase is "+str.toLowerCase());
+        print("Trimmed string is "+str.trim());
+        print("Replaced 'o' with 'a' is "+str.replace('o','a'));
+        print("Replaced 'World' with 'Java' is "+str.replace("World","Java"));
+        for (String s : str.split(" ")) {
+            print(s);
+        }
+        print("Checking if string starts with 'Hello' is "+str.startsWith("Hello"));
+        print("Checking if string ends with 'World' is "+str.endsWith("World"));
+        print("Checking if string contains 'World' is "+str.contains("World"));
+        print("Checking if string is empty is "+str.isEmpty());
+        print("Checking if string is equal to 'Hello World' is "+str.equals("Hello World"));
+        print("Checking if string is equal to 'Hello World' ignoring case is "+str.equalsIgnoreCase("hello world"));
+
+
     }
+
 }
